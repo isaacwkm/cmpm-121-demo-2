25c7fa08e809f6d859dc65b2689a81ba47bbd3d4
isaacwkm@gmail.com <isaacwkm@gmail.com>
2024-10-26 12:07:40 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "Gooooood Morning World";
+const APP_NAME = "Gooood Morning World";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const canvas = document.createElement("canvas");
+canvas.width = 256; // width of the canvas
+canvas.height = 256; // height of the canvas
+
+app.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+if (ctx) {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 256, 256);
+} else {
+    console.error("failed to get context");
+}
