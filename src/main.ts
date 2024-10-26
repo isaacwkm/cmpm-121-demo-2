05357573999f1403535d1c92006aa5fa2a664232
isaacwkm@gmail.com <isaacwkm@gmail.com>
2024-10-26 12:09:40 -0700

    Step 1 + added comments

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
 
+// Setting canvas dimensions
 const canvas = document.createElement("canvas");
-canvas.width = 256; // width of the canvas
-canvas.height = 256; // height of the canvas
+canvas.width = 256;
+canvas.height = 256;
 
+// Append canvas element to document
 app.appendChild(canvas);
-
 const ctx = canvas.getContext("2d");
 
+// Filling the rectangle to see it
 if (ctx) {
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, 256, 256);
