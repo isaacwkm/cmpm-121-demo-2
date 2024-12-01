c8953512e06e7a287a2f09f1a324b23bf6ae58db
isaacwkm@gmail.com <isaacwkm@gmail.com>
2024-12-01 13:25:00 -0800

    renamed function to be more accurate to what it does

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,32 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 
+
 // Creating Game title
 const title = document.createElement("h1");
 setTitle();
 
+
 // Append title to the app container
 app.appendChild(title);
+
 
 // Setting canvas dimensions
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 
+
 // Append canvas element to document
 app.append(canvas);
 const ctx = canvas.getContext("2d");
 
-// Filling the rectangle to see it
-fillCanvas();
+// Filling the rectangle with white
+clearCanvas();
+
+
+// Data Structures
+let strokes: { x: number; y: number }[][] = []; // each stroke is an array of points until the user lets go of the mouse
 
 let drawFlag: boolean = false;
 canvas.addEventListener("mousedown", () => (drawFlag = true));
@@ -47,7 +55,7 @@ createClearButton();
 // Add an event listener for the click event
 clearButton.addEventListener("click", function () {
     // Code to execute when button is clicked
-    fillCanvas();
+    clearCanvas();
   });
 
 
@@ -57,7 +65,7 @@ function setTitle(){
     title.style.marginTop = "10px";
 }
 
-function fillCanvas(){
+function clearCanvas(){
     if (ctx) {
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, 256, 256);
